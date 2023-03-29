48d3957d7140ac6422c83458227d5797517e9ca9
deveshi23 <126592806+deveshi23@users.noreply.github.com>
2023-03-28 19:14:53 -0500

    Update startPage.blade.php

--- SE_Project_HTML/startPage.blade.php
+++ SE_Project_HTML/startPage.blade.php
@@ -7,24 +7,43 @@
         <title>Home</title>
         <link rel="stylesheet" href="reservation.css">
     </head>
+    
+ <style>
+.center { 
+  margin-down: 10px;
+  position: absolute;
+  top: 50%;
+  -ms-transform: translateX(-50%);
+  transform: translateX(20%);
+}
+
+.btn1{
+	margin-right:100px
+}
+
+.btn2{
+	margin-left:100px
+}
+</style>
 
     <body>
 
 
         <div class = "header">
-            <h2 align="center">Continental</h2>
+            <h2 align="center" style="font-size:50px;">Continental</h2>
         </div>
 
         <div class="mid">
             <p></p>
             <br>
-            <h1 align="center">Welcome!</h1>
+            <h1 align="center" style="font-family:Lucida Handwriting;">Welcome!</h1>
             <br>
             <p>
-                <button class = "button button" onclick="window.location.href ='roomSearch.blade.php'">
+                <div class = "center">
+                <button class = "btn1" onclick="window.location.href ='roomSearch.blade.php'">
                     MAKE A RESERVATION
                 </button>
-                <button class = "button button" onclick="window.location.href ='findReservation.blade.php'">
+                <button class = "btn2" onclick="window.location.href ='findReservation.blade.php'">
                     VIEW CURRENT RESERVATION
                 </button>
             </p>
