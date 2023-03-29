e34bc38d0348d87a847e5b538cd043612be31afa
deveshi23 <126592806+deveshi23@users.noreply.github.com>
2023-03-29 14:22:31 -0500

    Update makeReservation.blade.php

--- SE_Project_HTML/makeReservation.blade.php
+++ SE_Project_HTML/makeReservation.blade.php
@@ -6,35 +6,34 @@
     <title>Make Reservation</title>
     <link rel="stylesheet" href="reservation.css">
 </head>
+
 <body>
 <div class="left-column">
-    <p>
-        "Hello"
-    </p>
+    
 </div>
 
 <div class = "header">
-    <h2 align="center">MyBusinessName</h2>
+    <h2 align="center" style="font-size:50px;">Continental</h2>
 </div>
 
 
 <div class="mid">
-    <div style="padding-left: 45%">
+    <div style="padding-left: 32%">
         <h1>Make a Reservation</h1>
     </div>
     <br><br>
     <p>
-
     </p>
 
 
     <div class="right-column">
-        <label for="resources">Resources Available:</label>
+        <label for="resources"><b>Resources Available:</b></label><br>
         <select id="resources" multiple>
             <option value="capacity">Capacity</option>
             <option value="accessibility">Handicap Accessible</option>
             <option value="wifi">Wifi</option>
             <option value="whiteboard">Whiteboard</option>
+            
 
         </select>
     </div>
@@ -42,21 +41,22 @@
     <div style = "overflow-y:auto;">
 
         <form action ="reservation_form.php" method="get">
-            <label for="email">Email:</label>
+            <label for="email"><b><br>Email:</b></label><br>
             <input type="text" id="email" name="email" value="" required><br>
             <br>
-            <br>
+            
 
-            <label for="start_time">Start date and time:</label><br>
+            <label for="start_time"><b>Start date and time:</b></label><br>
             <input type="datetime-local" id="start_time" name="start_time" required/>
             <br>
             <br>
-            <br>
-            <label for="end_time">End date and time:</label><br>
+            
+            <label for="end_time"><b>End date and time:</b></label><br>
             <input type="datetime-local" id="end_time" name="end_time" required/>
             <br>
             <br>
             <br>
+            
         </form>
         <div style="padding-left: 50%">
 
