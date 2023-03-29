7f9e1425a73737b3edf4d6756631d69d5a905a49
deveshi23 <126592806+deveshi23@users.noreply.github.com>
2023-03-29 13:40:46 -0500

    Update reservationInformation.blade.php

--- SE_Project_HTML/reservationInformation.blade.php
+++ SE_Project_HTML/reservationInformation.blade.php
@@ -6,15 +6,22 @@
   <title>Find Reservation</title>
   <link rel="stylesheet" href="reservation.css">
 </head>
+
+<style>
+.btn1{
+	margin-right:50px
+}
+</style>
+
 <body>
 
 <div class = "header">
-  <h2 align="center">MyBusinessName</h2>
+  <h2 align="center"style="font-size:50px;">Continental</h2>
 </div>
 
 
 <div class="mid">
-  <h1>FindReservation</h1>
+  <h1 align="center" style="font-family:Lucida Handwriting;">Find Reservation</h1>
   <h3>Room: </h3>
   <h3>Date: </h3>
   <h3>Time: </h3>
@@ -22,7 +29,7 @@
   <br>
   <!--  /*TODO: Add Delete Message to delete button*/-->
   <p>
-    <button class = "button button" onclick="window.location.href ='startPage.blade.php'">
+    <button class = "btn1" onclick="window.location.href ='startPage.blade.php'">
       Delete
     </button>
     <button class = "button button" onclick="window.location.href ='startPage.blade.php'">
